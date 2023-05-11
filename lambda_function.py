03b083c94a2deaef11c6fdd8686d5f9be7e47337
lventura <ss*UH@MyyY&uol4>
2023-05-11 16:02:55 -0300

    Inserting try except.

--- lambda_function.py
+++ lambda_function.py
@@ -1,29 +1,31 @@
 import boto3
 from log import log
+from botocore.exceptions import ClientError
 
 
 def lambda_handler(event, context):
-    s3 = boto3.client('s3')
+    s3_client = boto3.client('s3')
 
     record = event['Records'][0]
-
-    print('record', record)
 
     bucket = record['s3']['bucket']['name']
     key = record['s3']['object']['key']
 
-    print('bucket', bucket)
-    print('key', key)
+    try:
+        get_object_result = s3_client.get_object(Bucket=bucket, Key=key)
 
-    get_object_result = s3.get_object(Bucket=bucket, Key=key)
+        print('get_object_result {}', get_object_result)
 
-    mega_byte = 1024 * 1024
+        mega_byte = 1024 * 1024
 
-    if get_object_result['ContentLength'] > 1 * mega_byte:
-        log('Objeto muito grande')
+        if get_object_result['ContentLength'] > 1 * mega_byte:
+            log('Objeto muito grande')
 
-        return 'Objeto muito grande'
+            return 'Objeto muito grande'
 
-    log('Objeto de tamanho OK')
+        log('Objeto de tamanho OK')
 
-    return 'Objeto de tamanho OK'
+        return 'Objeto de tamanho OK'
+    except ClientError as e:
+        print(f'Error retrievent S3 object: {e}')
+        return None
