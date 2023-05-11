29be8b33a886fdb20d2455afdfc402d589f2a3a1
lventura <ss*UH@MyyY&uol4>
2023-05-11 13:58:09 -0300

    Getting objects on s3

--- lambda_function.py
+++ lambda_function.py
@@ -1,14 +1,24 @@
+import boto3
 import json
 from log import log
 
 def lambda_handler(event, context):
+    s3 = boto3.client('s3')
 
-    log('Event: ' + json.dumps(event))
+    record = event.Records[0]
 
-    return {
-        'statusCode': 200,
-        'body': '<html><body>Dados da requisicao {}</body></hmtl>'.format(json.dumps(event)),
-        'headers': {
-            "content-type": "text/html"
-        }
-    }
+    Bucket = record.s3.bucket.name
+    Key = record.s3.object.key
+
+    getObjectResult = s3.get_object(Bucket, Key)
+
+    mega_byte = 1024 * 1024
+
+    if (getObjectResult.ContentLength > 1 * mega_byte):
+        log('Objeto muito grande')
+        
+        return 'Objeto muito grande'
+
+    log('Objeto de tamanho OK')
+
+    return 'Objeto de tamanho OK'
