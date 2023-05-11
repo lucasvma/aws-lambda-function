d7a1fc02c01b84d1ad41f02b63ec04909ead0f85
lventura <ss*UH@MyyY&uol4>
2023-05-11 14:43:48 -0300

    Fixing the errors.

--- lambda_function.py
+++ lambda_function.py
@@ -1,22 +1,22 @@
 import boto3
-import json
 from log import log
+
 
 def lambda_handler(event, context):
     s3 = boto3.client('s3')
 
-    record = event.Records[0]
+    record = event.get()['Records'][0]
 
-    Bucket = record.s3.bucket.name
-    Key = record.s3.object.key
+    bucket = record.s3.bucket.name
+    key = record.s3.object.key
 
-    getObjectResult = s3.get_object(Bucket, Key)
+    get_object_result = s3.get_object(Bucket=bucket, Key=key)
 
     mega_byte = 1024 * 1024
 
-    if (getObjectResult.ContentLength > 1 * mega_byte):
+    if get_object_result.get()['ContentLength'] > 1 * mega_byte:
         log('Objeto muito grande')
-        
+
         return 'Objeto muito grande'
 
     log('Objeto de tamanho OK')
--- log.py
+++ log.py
@@ -1,6 +1,7 @@
 import json
 import os
 
+
 def log(message):
     print(os.environ['MINHA_VAR'])
     print(json.dumps(message))
